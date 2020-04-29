cc9c5101af7c58f7b9d9c01400ef0e7cd9ecbdd6
Tamme Schichler <tamme@schichler.dev>
2020-04-29 23:15:39 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use {atty::Stream, structopt::StructOpt};
+
+/// Simple-ish time tracking from the command line.
+///
+/// You can use a text editor to view/edit the tracking file. The parser should be lenient.
+#[derive(Debug, StructOpt)]
+#[structopt(author)]
+struct Options {
+    /// Enables colour.
+    #[structopt(short, long)]
+    color: bool,
+    /// Disables colour. Overrides --color.
+    #[structopt(short = "C", long)]
+    no_color: bool,
+    /// Disables searching parent directory chain for time tracking files.
+    #[structopt(short = "W", long)]
+    no_walk: bool,
+    #[structopt(subcommand)]
+    command: Option<Command>,
+    /// Either adds tags to the edited time (start, stop) or filters by them (, stats).
+    #[structopt(short, long)]
+    tags: Vec<String>,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Print current status information.
+    #[structopt(name = "")]
+    None,
+    /// Create a time tracking file (.tok-tracker) in the current working directory.
+    Init,
+    /// Start tracking time.
+    Start {
+        /// Adds a comment to the new time range.
+        #[structopt(short, long)]
+        comment: Option<String>,
+    },
+    /// Stop tracking time.
+    Stop {
+        /// Adds a comment to the finished time range.
+        #[structopt(short, long)]
+        comment: Option<String>,
+    },
+    /// Displays various tallies.
+    Stats,
+}
+
+fn main() {
+    let options = Options::from_args();
+
+    let _color = if options.no_color {
+        false
+    } else if options.color {
+        true
+    } else {
+        atty::is(Stream::Stdout)
+    };
+}
